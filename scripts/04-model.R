72dd404f6ed8228dbbc58517b871d3255d986f81
Sandy Yu <jingying.yu@mail.utoronto.ca>
2024-04-10 13:42:10 -0400

    finished simulating data and added preamble to model script

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: create bayesian model based on cleaned data
+# Author: Sandy Yu
+# Date: 2 April 2024
+# Contact: jingying.yu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: downloaded and cleaned data, understand what model to use
+
 
 
 #### Workspace setup ####
